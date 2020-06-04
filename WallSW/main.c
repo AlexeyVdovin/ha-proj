430fdbbaaa9fc4696e7f6daea4c884349dbaa6c7
Alexey Vdovin <av.nnov@gmail.com>
2020-06-04 23:16:58 +0300

    mqtt-c added

--- WallSW/main.c
+++ WallSW/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 #include "minini/minIni.h"
+#include "wiringPi/wiringPi.h"
+#include "mcp23017.h"
+#include "stm.h"
 
 #define array_sz(a)  (sizeof(a)/sizeof((a)[0]))
+char* cfg_name = "wallsw.conf";
 
 int main(int argc, char* argv[])
 {
@@ -11,9 +15,13 @@ int main(int argc, char* argv[])
     
     if(argc > 1)
     {
-        n = ini_gets(NULL, "string", "dummy", str, array_sz(str), argv[1]);
-        printf("cfg: '%s'\n", str);
+        cfg_name = argv[1];
     }
+    n = ini_gets("mqtt", "ip", "127.0.0.1", str, array_sz(str), cfg_name);
+    printf("ip: '%s'\n", str);
+
+    n = ini_gets("mqtt", "port", "1883", str, array_sz(str), cfg_name);
+    printf("port: '%s'\n", str);
 
     return 0;
 }
